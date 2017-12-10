465ecbd89af4d27390ffcba13625a309c7c6b22b
fradia <francesca.diana.3@gmail.com>
2017-12-10 17:33:34 +0100

    projects_forked_2017

---
+++ ALS/data/projects_forked_2017.sql
@@ -0,0 +1,3 @@
+SELECT id, owner_id,name,language,created_at, forked_from, deleted, forked_from_commit_id
+FROM [ghtorrent-bq:ght_2017_09_01.projects] 
+WHERE YEAR(created_at)=2017 AND forked_from IS NOT NULL
